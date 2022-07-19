4b3630a3240d9b1e6cecda2d5d18415e88cf45cb
GeigerTiger55 <elise.geiger@gmail.com>
2022-07-19 15:30:06 -0700

    Installed axios and created files for components

---
+++ src/JobCard.js
@@ -0,0 +1,13 @@
+/** JobCard for presenting job infor
+ *
+ * Props:
+ * - job: {title, salary, equity, company...}
+ *
+ * JobCardList -> JobCard
+ */
+
+function JobCard(){
+  return <header>JOB CARD</header>;
+}
+
+export default JobCard;
