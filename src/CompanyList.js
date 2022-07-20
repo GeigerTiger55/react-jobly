e45462cb805ec8ccf2c312101f668b998308926b
Clair-Hawkes <hawkesj7@gmail.com>
2022-07-20 11:15:31 -0700

    Add the companiesList component

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -2,34 +2,60 @@ import CompanyCard from './CompanyCard';
 import SearchForm from './SearchForm';
 import JoblyApi from './api';
 
+import {useState, useEffect} from 'react';
+
+
 /** CompanyList for displaying companies
  *
- * Props:
- * - companies: [{company}, ...]
+ * Props: None
  *
  * State:
+ * - companies: [{company}, ...]
  * - searchTerms
  *
  * Routes -> CompanyList -> { SearchForm, CompanyCard }
  */
 
 function CompanyList() {
+  const [companies, setCompanies] = useState({
+    data: [],
+    isLoading: true,
+  });
+  const [searchTerms, setSearchTerms] = useState();
+  //TODO: setSearchTerms for searchBar
+  console.log("CompanyList",companies,searchTerms);
 
-  const companies = JoblyApi.getCompanies("Anderson");
-  // console.log('CompanyList component',companies);
 
-  // TODO: useState()
-  // TODO: useEffect()
+  useEffect(function fetchCompaniesOnSearchTermsChange() {
+    async function fetchCompanies() {
+      const companiesResult = await JoblyApi.getCompanies(searchTerms)
+      setCompanies({
+        data: companiesResult,
+        isLoading: false
+      });
+    }
+    fetchCompanies();
+  }, [searchTerms]);
 
 
   return (
     <div>
       <header>COMPANY LISTER</header>
-
       <SearchForm />
-      <CompanyCard />
+      {companies.isLoading ?
+      <p>Loading!</p>
+      :
+      companies.data.map(
+        company => <CompanyCard key={company.handle} company={company}/>
+        )
+      }
     </div>
   );
 }
 
 export default CompanyList;
+
+
+// THinking Space:
+// Have to get the props
+// Map the props to create
