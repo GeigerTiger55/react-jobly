b1d6b4d8124463564ec13320da6c7bc9b9b653e1
GeigerTiger55 <elise.geiger@gmail.com>
2022-07-22 14:23:11 -0700

    Added homepage conditional for if user is logged in and imported bootstrap

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import userContext from './userContext';
+import { Link } from "react-router-dom";
 
 /** Homepage for landing and redirects (presentational)
  *
@@ -12,8 +13,29 @@ import userContext from './userContext';
  * Routes -> Homepage
  */
 
- function Homepage(){
-  return <header>HOMEPAGE</header>;
+function Homepage() {
+  const { userData } = useContext(userContext);
+
+  if (userData.token) {
+    return (
+      <div>
+        <h1>Jobly</h1>
+        <h4>All the jobs in one, convenient place.</h4>
+        <h1>WelcomeBack, {userData.user.firstName}</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Jobly</h1>
+        <h4>All the jobs in one, convenient place.</h4>
+        <div>
+          <Link to='/login' className='btn btn-primary'> Login </Link>
+          <Link to='/signup' className='btn btn-primary'> Sign Up </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Homepage;
